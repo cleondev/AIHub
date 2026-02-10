243eae94bf57ef663b9d4181d1e447b63f9150e2
cleondev <sudosonlh@gmail.com>
2026-02-10 17:51:51 +0700

    Fix AppHost project registration without generated Projects types

--- src/AIHub.AppHost/Program.cs
+++ src/AIHub.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var chatbotApi = builder.AddProject<Projects.AIHub_Api>("chatbot-api");
-var productApi = builder.AddProject<Projects.AIHub_ProductApi>("product-api");
+var chatbotApi = builder.AddProject("chatbot-api", "../AIHub.Api/AIHub.Api.csproj");
+var productApi = builder.AddProject("product-api", "../AIHub.ProductApi/AIHub.ProductApi.csproj");
 
 chatbotApi.WithReference(productApi);
 
